9c04010e99d549c6590cd31671057a2384565772
tlsande <tlsande@g.clemson.edu>
2021-01-10 23:40:34 -0600

    Added getSchemaForGame API call

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,18 +32,37 @@ namespace Game_Stats_Tracker
 
         private void testButton_Click(object sender, RoutedEventArgs e)
         {
-            player testData = playerAchievements.getPlayerAchievements(appData.getKey("steam"), "76561198000713602", "211420", "EN");
+            //player testData = playerAchievements.getPlayerAchievements(appData.getKey("steam"), "76561198000713602", "211420", "EN");
 
-            testTextBox.Text += testData.playertsats.steamID + "\n";
-            testTextBox.Text += testData.playertsats.game + "\n";
-            foreach (achievement i in testData.playertsats.achievements)
+            //testTextBox.Text += testData.playertsats.steamID + "\n";
+            //testTextBox.Text += testData.playertsats.game + "\n";
+            //foreach (achievement i in testData.playertsats.achievements)
+            //{
+            //    testTextBox.Text += "   " + i.apiname + "\n";
+            //    testTextBox.Text += "   " + i.achieved + "\n";
+            //    testTextBox.Text += "   " + i.unlockedTime + "\n";
+            //    testTextBox.Text += "   " + i.name + "\n";
+            //    testTextBox.Text += "   " + i.description + "\n";
+            //}
+
+            //string testData2 = schemaForGame.getSchemaForGame(appData.getKey("steam"), "211420", "EN");
+            //testTextBox.Text += testData2;
+
+            gameInfo testData2 = schemaForGame.getSchemaForGame(appData.getKey("steam"), "211420", "EN");
+
+            testTextBox.Text += testData2.game.gameName + "\n";
+            testTextBox.Text += testData2.game.version + "\n";
+            foreach (achievementInfo i in testData2.game.gameStats.achievements)
             {
-                testTextBox.Text += "   " + i.apiname + "\n";
-                testTextBox.Text += "   " + i.achieved + "\n";
-                testTextBox.Text += "   " + i.unlockedTime + "\n";
                 testTextBox.Text += "   " + i.name + "\n";
+                testTextBox.Text += "   " + i.defaultvalue + "\n";
+                testTextBox.Text += "   " + i.displayName + "\n";
+                testTextBox.Text += "   " + i.hidden + "\n";
                 testTextBox.Text += "   " + i.description + "\n";
+                testTextBox.Text += "   " + i.iconURL + "\n";
+                testTextBox.Text += "   " + i.icongreyURL + "\n";
             }
+
 
             //testTextBox.Text += System.IO.Path.Combine(Directory.GetCurrentDirectory(), "\\data\\keys.xml");
             //testTextBox.Text += System.IO.Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.FullName, @"data\", "keys.xml");
---
+++ api calls/steam/schemaForGame.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_Stats_Tracker.api_calls.steam
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class achievementInfo
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("defaultvalue")]
+        public string defaultvalue { get; set; }
+
+        [JsonProperty("displayName")]
+        public string displayName { get; set; }
+
+        [JsonProperty("hidden")]
+        public bool hidden { get; set; }
+
+        [JsonProperty("description")]
+        public string description { get; set; }
+
+        [JsonProperty("icon")]
+        public string iconURL { get; set; }
+
+        [JsonProperty("icongray")]
+        public string icongreyURL { get; set; }
+    }
+    [JsonObject(MemberSerialization.OptIn)]
+    public class stats
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("defaultvalue")]
+        public string defaultValue { get; set; }
+
+        [JsonProperty("displayName")]
+        public string displayName { get; set; }
+    }
+    [JsonObject(MemberSerialization.OptIn)]
+    public class gameStats
+    {
+        [JsonProperty("achievements")]
+        public IList<achievementInfo> achievements { get; set; }
+
+        [JsonProperty("stats")]
+        public IList<stats> stats { get; set; }
+    }
+    [JsonObject(MemberSerialization.OptIn)]
+    public class game
+    {
+        [JsonProperty("gameName")]
+        public string gameName { get; set; }
+
+        [JsonProperty("gameVersion")]
+        public string version { get; set; }
+
+        [JsonProperty("availableGameStats")]
+        public gameStats gameStats { get; set; }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class gameInfo
+    {
+        [JsonProperty("game")]
+        public game game { get; set; }
+    }
+    class schemaForGame
+    {
+        public static gameInfo getSchemaForGame(string apiKey, string appID, string language)
+        //public static string getSchemaForGame(string apiKey, string appID, string language)
+        {
+            var client = new RestClient("http://api.steampowered.com");
+            var request = new RestRequest("ISteamUserStats/GetSchemaForGame/v2/");
+            request.AddParameter("key", apiKey, ParameterType.QueryString);
+            request.AddParameter("appid", appID, ParameterType.QueryString);
+            request.AddParameter("l", language, ParameterType.QueryString);
+            var response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<gameInfo>(response.Content);
+            //return response.Content;
+        }
+    }
+}
